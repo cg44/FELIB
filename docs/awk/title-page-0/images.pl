f3e57a8abdbd49ecaf98d8e3d320bac2de0f8f91
cg44 <cg44@f5a69418-6ad0-45aa-8498-9526bd39ce99>
2016-09-02 15:12:40 +0000

    Initial version of awk

---
+++ docs/awk/title-page-0/images.pl
@@ -0,0 +1,12 @@
+# LaTeX2HTML 99.1 release (March 30, 1999)
+# Associate images original text with physical files.
+
+
+$key = q/L^2;MSF=1.4;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="19" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ SRC="img2.gif"
+ ALT="$L^2$">|; 
+
+1;
+
