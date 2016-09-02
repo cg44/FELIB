588779b166aae8e811b51ca69a3d139c624a9339
cg44 <cg44@f5a69418-6ad0-45aa-8498-9526bd39ce99>
2016-09-02 14:23:32 +0000

    Initial version of reports

---
+++ reports/re-engineering/design/images.pl
@@ -0,0 +1,12 @@
+# LaTeX2HTML 2K.1beta (1.47)
+# Associate images original text with physical files.
+
+
+$key = q/1+epsilon>1;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="75" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$1+\epsilon&gt;1$">|; 
+
+1;
+
